f1eecf8dc54b9816bd529580437572da2dca6d19
Anish Karandikar <anishkny@gmail.com>
2017-04-21 23:44:50 -0400

    Updated README and Firebase keys

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 import Vuex from 'vuex';
-import App from './App.vue'
-import MainPage from './MainPage.vue'
-import OtherPage from './OtherPage.vue'
-import LoginPage from './LoginPage.vue'
+import App from './App.vue';
+import MainPage from './MainPage.vue';
+import OtherPage from './OtherPage.vue';
+import LoginPage from './LoginPage.vue';
 import firebase from 'firebase';
 
 console.clear();
 console.log(new Date());
 
+// TODO: Replace the following with your own keys from firebase.google.com
 firebase.initializeApp({
-  apiKey: "AIzaSyALtQZnNF14_btkXzeJh7mhTc515aDw3Qw",
-  authDomain: "namasteworld-51f82.firebaseapp.com",
-  databaseURL: "https://namasteworld-51f82.firebaseio.com",
-  projectId: "namasteworld-51f82",
-  storageBucket: "namasteworld-51f82.appspot.com",
-  messagingSenderId: "899946482077"
+  apiKey: "0ab96f46af51",
+  authDomain: "abc027e0-51f82.firebaseapp.com",
+  databaseURL: "https://abc027e0-51f82.firebaseio.com",
+  projectId: "abc027e0-51f82",
+  storageBucket: "abc027e0-51f82.appspot.com",
+  messagingSenderId: "677d"
 });
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
-
 
 new Vue({
   el: '#app',
